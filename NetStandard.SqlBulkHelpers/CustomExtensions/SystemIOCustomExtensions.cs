3f8536a62a7e077c873234846255b42ed38f6e0b
CajunCoding <brandon@cajuncoding.com>
2022-11-18 16:05:05 -0600

    WIP! - Finished drafting MaterializedDataScriptBuilder script handlers for Cloning table... - Factored out the Table Schema query as now it's a very large query to structure the JSON and retrieve all relevant data, so now we can maintain it as an embedded resource which also enables syntax highlighting, etc. - Various other code cleanup, fixes, etc.

---
+++ NetStandard.SqlBulkHelpers/CustomExtensions/SystemIOCustomExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SqlBulkHelpers.CustomExtensions
+{
+    internal static class SystemIOCustomExtensions
+    {
+        public static byte[] ToByteArray(this Stream stream)
+        {
+            byte[] bytes = null;
+            if (stream is MemoryStream)
+            {
+                //Memory stream is easy to work with and natively supports converting to ByteArray.
+                var memoryStream = stream as MemoryStream;
+                bytes = memoryStream.ToArray();
+            }
+            else
+            {
+                //For all other stream types we need to validate that we can Read Them!
+                if (!stream.CanRead) throw new ArgumentException("Stream specified does not support Read operations.");
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+            }
+
+            return bytes;
+        }
+
+        public static async Task<byte[]> ToByteArrayAsync(this Stream stream)
+        {
+            byte[] bytes;
+            if (stream is MemoryStream existingMemoryStream)
+            {
+                //Memory stream is easy to work with and natively supports converting to ByteArray.
+                bytes = existingMemoryStream.ToArray();
+            }
+            else
+            {
+                //For all other stream types we need to validate that we can Read Them!
+                if (!stream.CanRead) throw new ArgumentException("Stream specified does not support Read operations.");
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    await stream.CopyToAsync(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+            }
+
+            return bytes;
+        }
+    }
+}
